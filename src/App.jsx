b566323d0875912ed0f64dba06fbb50ee00c896d
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-12-06 19:49:28 -0800

    built complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import React, { useState } from "react";
-
+import DisplayIdeas from "./Components/DisplayIdeas"
+import IdeasForm from "./Components/IdeasForm"
 
 function App() {
   const [ideas, setIdeas] = useState([
     "Movie about a guy who gets bit by a radioactive mole rat",
     "Romcom about two developers over Zoom",
     "Horror film where AWS goes down for 24 hours"
-  ]);
-
-
-
+  ]); 
+ 
+  const addIdeas = (newIdeas) => {
+    setIdeas([...ideas, newIdeas])
+  }
   return (
     <main>
       <h1>Welcome, Super Awesome Movie Monster Think Tank!</h1>
-
+      <DisplayIdeas ideas = {ideas} />
       <br />
       
       <h1>Submit your ideas down below:</h1>
-
+      <IdeasForm addIdeas={addIdeas} />
 
     </main>
   );
